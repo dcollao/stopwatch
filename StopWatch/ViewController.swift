f78dbe0280a708cda31be0c1acca2fa2a09a1164
Diego  Collao <dcollao@icloud.com>
2016-07-25 23:12:30 -0400

    asd

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -9,15 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var timer = Timer()
+    var count = 0
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var playButton: UIBarButtonItem!
+    @IBOutlet weak var pauseButton: UIBarButtonItem!
+    @IBOutlet weak var stopButton: UIBarButtonItem!
+    
+    @IBAction func playAction(_ sender: AnyObject) {
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateTime), userInfo: nil, repeats: true )
+    }
+    
+    @IBAction func stopAction(_ sender: AnyObject) {
+        timer.invalidate()
+        count = 0
+        resultLabel.text = "Start!"
+    }
+    
+    @IBAction func pauseAction(_ sender: AnyObject) {
+        timer.invalidate()
+    }
+    
+    func updateTime() {
+        count += 1
+        resultLabel.text = "\(count)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
